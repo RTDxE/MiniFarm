a3183b26bf98de687eb89d7744b65f17aa39861f
Timofffee <timofffee@gmail.com>
2024-04-14 21:01:10 +0400

    Breakin update

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,9 +4,6 @@ namespace MiniFarm.Player
 {
     public class Player: MonoBehaviour
     {
-        // TODO только для теста. УДОЛИТЬ
-        [SerializeField] private GameObject garden;
-
         [SerializeField] private PlayerMove _playerMove;
         public PlayerMove PlayerMove => _playerMove;
         
@@ -16,23 +13,5 @@ namespace MiniFarm.Player
         [SerializeField] private PlayerInventory _playerInventory;
         public PlayerInventory PlayerInventory => _playerInventory;
         
-        private void Start()
-        {
-            // TODO только для теста. УДОЛИТЬ
-            {
-                var gardenInst = Instantiate(garden);
-                ObjectsManager.Instance.PlaceObject(gardenInst);
-            }
-            {
-                var gardenInst = Instantiate(garden);
-                gardenInst.transform.position = new Vector3(1.4f, 0, 0);
-                ObjectsManager.Instance.PlaceObject(gardenInst);
-            }
-            {
-                var gardenInst = Instantiate(garden);
-                gardenInst.transform.position = new Vector3(1.4f, 1.1f, 0);
-                ObjectsManager.Instance.PlaceObject(gardenInst);
-            }
-        }
     }
 }
--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace MiniFarm
@@ -8,6 +9,9 @@ namespace MiniFarm
     {
         private Player.Player _player;
         private InteractableObject _lastObject;
+
+        private bool inShopArea;
+        [SerializeField] private InteractableObject _shop;
 
         [SerializeField] private UnityEvent onEnterInteract;
         [SerializeField] private UnityEvent onExitInteract;
@@ -19,6 +23,12 @@ namespace MiniFarm
 
         private void Update()
         {
+            if (inShopArea)
+            {
+                if (_lastObject != _shop)
+                    _lastObject = _shop;
+                return;
+            }
             var gObject = ObjectsManager.Instance.GetObject(transform.position);
             InteractableObject obj;
             if (gObject == null || !gObject.TryGetComponent(out obj))
@@ -29,7 +39,7 @@ namespace MiniFarm
                 onExitInteract.Invoke();
                 return;
             }
-            
+
             if (obj != _lastObject && _lastObject != null)
             {
                 _lastObject.Exit(_player);
@@ -39,7 +49,7 @@ namespace MiniFarm
             {
                 onEnterInteract.Invoke();
             }
-            
+
             _lastObject = obj;
             _lastObject.Enter(_player);
         }
@@ -47,6 +57,55 @@ namespace MiniFarm
         public void Interact()
         {
             _lastObject.Interact(_player);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            inShopArea = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            _lastObject = null;
+            inShopArea = false;
+        }
+    }
+
+    public class MyItem
+    {
+        public string name;
+    }
+
+    public class MyToolItem : MyItem
+    {
+        public int durability;
+    }
+    
+    public class MyNewToolItem : MyToolItem
+    {
+        public bool isNew;
+    }
+
+    public class RandomClass
+    {
+        public void SomeDo(MyItem item) // item -> MyToolItem
+        {
+            // if (item.GetType() == typeof(MyToolItem)) {}
+            
+            // (item as MyToolItem).durability;
+            // ((MyToolItem)item).durability;
+
+            // if (item is MyToolItem toolItem)
+            // {
+            //     item.name;
+            //     toolItem.durability;
+            // }
+        }
+
+        public void NoMore(MyNewToolItem item)
+        {
+            // item type == MyItem
+            // typeof(MyItem).IsInstanceOfType(item)
         }
     }
 }
